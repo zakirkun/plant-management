38c370a0c305a0a1dc36666bc0381e8698ea969a
ed-roh <edwardroh89@gmail.com>
2024-08-08 09:45:27 -0700

    init

---
+++ server/prisma/seed.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import path from "path";
+const prisma = new PrismaClient();
+
+async function deleteAllData(orderedFileNames: string[]) {
+  const modelNames = orderedFileNames.map((fileName) => {
+    const modelName = path.basename(fileName, path.extname(fileName));
+    return modelName.charAt(0).toUpperCase() + modelName.slice(1);
+  });
+
+  for (const modelName of modelNames) {
+    const model: any = prisma[modelName as keyof typeof prisma];
+    await model.deleteMany({});
+    console.log(`Cleared data from ${modelName}`);
+  }
+}
+
+async function main() {
+  const dataDirectory = path.join(__dirname, "seedData");
+
+  const orderedFileNames = [
+    "team.json",
+    "project.json",
+    "projectTeam.json",
+    "user.json",
+    "task.json",
+    "attachment.json",
+    "comment.json",
+    "taskAssignment.json",
+  ];
+
+  await deleteAllData(orderedFileNames);
+
+  for (const fileName of orderedFileNames) {
+    const filePath = path.join(dataDirectory, fileName);
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const modelName = path.basename(fileName, path.extname(fileName));
+    const model: any = prisma[modelName as keyof typeof prisma];
+
+    for (const data of jsonData) {
+      await model.create({ data });
+    }
+
+    console.log(`Seeded ${modelName} with data from ${fileName}`);
+  }
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect());
